3d80e510b9ca79a4fb584b55780b8928ffe5d1c4
NoelleWeaver <141285591+NoelleWeaver@users.noreply.github.com>
2025-01-10 15:17:23 -0700

    Almost finished navbar only finish logo

--- drifted/src/components/NavBar.jsx
+++ drifted/src/components/NavBar.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import '../pages/css/navbar.css'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className="navbarContainer">
+      <img className="navbarLogo" src="https://res.cloudinary.com/dynkv0p8x/image/upload/v1736472515/paper-plane_lpbunt.png"/>
+      <div className="navbarLinks">
+        <a href="/">Home</a>
+        <a href="/blog">Blog</a>
+        <a href="/about">About</a>
+      </div>
+      <div className="searchContainer">
+        <button className='searchButton'>
+          <img className='searchImg' src="https://res.cloudinary.com/dynkv0p8x/image/upload/v1736472517/search_hnuf23.png" />
+        </button>
+        <input className='searchInput' type="text" placeholder="Search..." />
+      </div>
+      <div className="navbarLineBreak"></div>
+    </div>
   )
 }
 
--- drifted/src/pages/main.jsx
+++ drifted/src/pages/main.jsx
@@ -2,14 +2,16 @@ import { useState } from "react";
 import './css/main.css';
 import React from 'react'
 import Footer from "../components/Footer";
+import Navbar from "../components/NavBar";
 
 export const main = () => {
   return (
     <>
+    <Navbar className="navbar" />
       <div className="top-container">
         <img className="main" src="https://res.cloudinary.com/dynkv0p8x/image/upload/v1736469296/mountain_wskmaf.jpg" alt="Mountains" />
       </div>
-
+      
       <div className="blog-container">
         <h1 className="places-title">Places</h1>
       </div>
