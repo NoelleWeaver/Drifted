a6e16c4438d9785ce7593f073fc4f7f473b9dec9
NoelleWeaver <141285591+NoelleWeaver@users.noreply.github.com>
2025-01-14 00:03:11 -0700

    i dont wanna do this anymore 😿

--- drifted/src/pages/blog.jsx
+++ drifted/src/pages/blog.jsx
@@ -2,11 +2,13 @@ import './App.css';
 import Comments from "../components/Comments"
 import Footer from "../components/Footer"
 import SearchBar from "../components/SearchBar"
+import Gallery from "../components/Gallery"
 
 function App() {
   return (
     <div>
       <SearchBar />
+      <Gallery />
       <Comments currentUserId="1" />
       <Footer />
     </div>
